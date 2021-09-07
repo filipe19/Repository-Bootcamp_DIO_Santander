0305b5272b971dab6504e87a798983324efa2e94
Fillipe_Alves <31691707+filipe19@users.noreply.github.com>
2021-09-07 14:29:01 -0300

    up front end

---
+++ Gerenciador-salas-reunioes-Java-Angular_/Back-End/saladereuniao/src/main/java/com/dio/saladereuniao/saladereuniao/repository/RoomRepository.java
@@ -0,0 +1,10 @@
+package com.dio.saladereuniao.saladereuniao.repository;
+
+import com.dio.saladereuniao.saladereuniao.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room,Long> {
+
+}
